036ef2c8dfe57cad4a9ecef4574f1ac8c7b7e715
azdine-dev <azeddinebenhalima@gmail.com>
2020-07-25 13:47:37 +0100

    first commit

---
+++ src/main/java/com/azdine/dev/touristguider/TouristGuiderApplication.java
@@ -0,0 +1,13 @@
+package com.azdine.dev.touristguider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TouristGuiderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TouristGuiderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/azdine/dev/touristguider/TouristGuiderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.azdine.dev.touristguider;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TouristGuiderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
